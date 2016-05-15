61f188b5b2e3566c3e9919f49c270e0dcaef1b81
german gomez <germangb42@gmail.com>
2016-05-15 18:13:22 +0200

    decal reflections

--- Application/src/activity/LoverLocation.java
+++ Application/src/activity/LoverLocation.java
@@ -261,6 +261,7 @@ public class LoverLocation extends LocationActivity{
         /**Set camera position**/
         float aspect = (float) context.window.getWidth() / context.window.getHeight();
         camera.projection.setPerspective((float) Math.toRadians(45), aspect, 0.01f, 100f);
+        camera.clearColor.set(0.85f);
         cameraReflect.projection.setPerspective((float) Math.toRadians(45), aspect, 0.01f, 100f);
     }
 
