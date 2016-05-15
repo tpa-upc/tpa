c49dd82570cd573f90b16dec775f840e5ed27009
german gomez <germangb42@gmail.com>
2016-05-15 18:44:14 +0200

    post merge

--- Application/src/test/GameTest.java
+++ Application/src/test/GameTest.java
@@ -61,12 +61,11 @@ public class GameTest implements Application {
                 // push some activity
                 //Game.getInstance().pushActivity(GameActivity.Room);
                 //Game.getInstance().pushActivity(GameActivity.Interrogation);
-                //Game.getInstance().pushActivity(GameActivity.Interrogation);
                 //Game.getInstance().pushActivity(GameActivity.Acid);
                 //Game.getInstance().pushActivity(GameActivity.Intro);
-                //Game.getInstance().pushActivity(GameActivity.Club);
+                Game.getInstance().pushActivity(GameActivity.Club);
                 //Game.getInstance().pushActivity(GameActivity.Interrogation);
-                Game.getInstance().pushActivity(GameActivity.Lover);
+                //Game.getInstance().pushActivity(GameActivity.Lover);
             }
 
             context.renderer.beginFrame();
